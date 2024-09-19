29ed9a3ff1cf595182feadc3f2dab547f83c75ff
Izpodvypodverta <izpodvypodwerta@gmail.com>
2024-09-19 15:53:46 +0300

    remove simplegrid css class

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,12 +1,10 @@
-import { Card, Group, Image, SimpleGrid, Text } from '@mantine/core';
+import { SimpleGrid } from '@mantine/core';
 import { mockData } from '@/utils/mock';
 import MyCard from './Card';
-import classes from './Card.module.css';
 
 const Cards = () => {
   return (
     <SimpleGrid
-      className={classes.simpleGrid}
       cols={{ base: 1, sm: 2, lg: 3 }}
       spacing={{ base: 'md', sm: 'xl' }}
       verticalSpacing={{ base: 'md', sm: 'xl' }}
